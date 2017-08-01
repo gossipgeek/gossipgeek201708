34f8ea9b48ba767879838e364a5b6546745995fc
Gotogoo <fcliang@thoughtworks.com>
2017-08-01 20:24:38 +0800

    connect LeanCloud App Success

---
+++ GossipGeek/GossipGeek/SignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.h
+//  GossipGeek
+//
+//  Created by Facheng Liang  on 01/08/2017.
+//  Copyright © 2017 cozhang . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignInViewController : UIViewController
+
+@end
