831fdce120d41ccf1a608229c351504c03ef7f05
bailitusu <231820801@qq.com>
2017-08-01 15:07:11 +0800

    create project

---
+++ GossipGeek/GossipGeek/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GossipGeek
+//
+//  Created by cozhang  on 01/08/2017.
+//  Copyright © 2017 cozhang . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
