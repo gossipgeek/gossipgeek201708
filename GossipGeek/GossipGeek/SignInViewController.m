e425cb3972ca5647dd2ca9546ce279f0ffb8f376
Gotogoo <fcliang@thoughtworks.com>
2017-08-02 17:46:10 +0800

    delete unuseful code

--- GossipGeek/GossipGeek/SignInViewController.m
+++ GossipGeek/GossipGeek/SignInViewController.m
@@ -17,32 +17,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+   
     
-    /* 测试
-    AVObject *user = [AVObject objectWithClassName:@"userTest"];
-    [user setObject:@"fcliang@thoughtworks.com"    forKey:@"email"];
-    [user setObject:@"admin" forKey:@"userName"];
-    [user setObject:@"admin" forKey:@"password"];
-    [user save];
-     */
 
   
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+   
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
